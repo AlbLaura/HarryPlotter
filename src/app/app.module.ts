f1e7a14e9c9fd0491c6008596c6c0d413af21ec8
AlbLaura <albornoz.laurasol@gmail.com>
2023-05-26 19:20:20 -0300

    agregado del componente noticia/ultimas noticias, carousel y home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,10 @@ import { ProductComponent } from './components/product/product.component';
 import { LatestProductComponent } from './components/latest-product/latest-product.component';
 import { ListProductComponent } from './components/list-product/list-product.component';
 import { QuestionComponent } from './components/question/question.component';
+import { HomeComponent } from './home/home.component';
+import { CarrouselComponent } from './components/carrousel/carrousel.component';
+import { LatestAnnouncementComponent } from './components/latest-announcement/latest-announcement.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +28,11 @@ import { QuestionComponent } from './components/question/question.component';
     ProductComponent,
     LatestProductComponent,
     ListProductComponent,
-    QuestionComponent
+    QuestionComponent,
+    HomeComponent,
+    CarrouselComponent,
+    LatestAnnouncementComponent,
+    AnnouncementComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/latest-announcement/latest-announcement.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-latest-announcement',
+  templateUrl: './latest-announcement.component.html',
+  styleUrls: ['./latest-announcement.component.css']
+})
+export class LatestAnnouncementComponent {
+
+}
