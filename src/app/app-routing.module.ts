0f5f5398156b9e07eb3c2d9980df0aa365685577
AlbLaura <albornoz.laurasol@gmail.com>
2023-10-12 10:27:46 -0300

    11/oct routing y estilizacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoriasComponent } from './categorias/categorias.component';
+import { NewsComponent } from './news/news.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductComponent } from './components/product/product.component';
+import { AnnouncementComponent } from './components/announcement/announcement.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'home-component', component: HomeComponent },
+  { path: 'categorias-component', component: CategoriasComponent},
+  { path: 'news-component', component: NewsComponent},
+  { path: 'contact-component', component: ContactComponent},
+  { path: 'login-component', component: LoginComponent},
+  { path: 'cart-component', component: CartComponent},
+  { path: 'product-component', component: ProductComponent},
+  { path: 'announcement-component', component: AnnouncementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/components/pagination/pagination.component.ts
+++ src/app/components/pagination/pagination.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./pagination.component.css']
 })
 export class PaginationComponent {
+// Hace falta implementar un service con el listado de productos total
 
+  /* public page: Number = 1;
+  public totalPages: Number = 10;
+  private numResults: number = 10;
+
+  constructor() {
+  } */
 }
